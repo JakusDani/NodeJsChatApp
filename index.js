9dbca67a7ecd86b170c9685f8b9b485300c35b57
JakusDani <skelox0@gmail.com>
2021-06-02 23:32:06 +0200

    change port -> PORT

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(process.env.port || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('Server listening on: 3000');
 });
